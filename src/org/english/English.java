1f8f0365d95ccd6991c634a2089b33cb0727c3c0
GaneshBaskie <ganeshbaskar18@gmail.com>
2021-05-31 22:26:06 +0530

    31/5/2021 projectwork

---
+++ src/org/english/English.java
@@ -0,0 +1,10 @@
+package org.english;
+
+import org.telgu.*;
+
+public class English extends Telgu {
+
+	public void englishLanguage () {
+		System.out.println("English");
+	}
+}
---
+++ src/org/tamil/Tamil.java
@@ -0,0 +1,11 @@
+package org.tamil;
+
+import org.english.*;
+
+public class Tamil extends English {
+	
+	public void tamilLanguage () {
+		System.out.println("Tamil");
+	}
+
+}
